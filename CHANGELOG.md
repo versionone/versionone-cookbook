ffab618c0d8fb146c8ebd8238500c73a489efe7e
Josh Gough <jsgough@gmail.com>
2014-04-30 17:00:19 -0400

    Creating a brand new Berkshelfified versionone-cookbook

--- CHANGELOG.md
+++
@@ -1,9 +0,0 @@
-versionone CHANGELOG
-====================
-
-This file is used to list changes made in each version of the versionone cookbook.
-
-0.1.0
------
-- [jkoberg] - Initial release of versionone
-
--- attributes/default.rb
+++
@@ -1,4 +0,0 @@
-
-
-default["versionone"]["sqlserver_authmode_regkey"] = "HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Microsoft SQL Server\\MSSQL11.MSSQLSERVER\\MSSQLServer"
-default["versionone"]["choc_private_feed"] = "https://www.myget.org/F/versionone/"
